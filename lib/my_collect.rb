8b5d228241ab7afcefd3f213e1180fb8f28e4164
Lizzie Chan <lchan217@gmail.com>
2019-02-14 15:10:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0 
   new_array = [] 
   while collection.length > i 
-    yield new_array << collection[i]
+    new_array << collection[i]
     i+=1 
   end 
   new_array
